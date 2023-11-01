d3bdc209c7d88d7ab23757d51137911955ecf716
Neal Magee <nmagee@users.noreply.github.com>
2023-11-01 14:25:55 -0400

    Update run-instance.sh

--- ec2/cli/run-instance.sh
+++ ec2/cli/run-instance.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+# ami - amazon linux 6.1
+
 set -e
 
 aws --profile cs4740 ec2 run-instances \
-  --image-id ami-05fa00d4c63e32376 \
+  --image-id ami-01bc990364452ab3e \
   --instance-type t2.micro \
   --key-name cs4740 \
   --security-group-ids sg-07fd99f31186c2019 \
