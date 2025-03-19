54d987a8554368720ba56db269b3b4d702409af5
Neal Magee <nem2p@virginia.edu>
2025-03-19 15:03:21 -0400

    Tracking improvements to SQS send

--- sqs/python3/send-message.py
+++ sqs/python3/send-message.py
@@ -5,16 +5,28 @@ import botocore
 from botocore.exceptions import ClientError
 
 sqs = boto3.client('sqs')
-qurl = "https://sqs.us-east-1.amazonaws.com/440848399208/video-queue"
+# qurl = "https://sqs.us-east-1.amazonaws.com/440848399208/video-queue"
+qurl = "https://sqs.us-east-1.amazonaws.com/379111353384/ds2002"
 
 # Send a message to the queue
 def send_message(qurl, message):
     try:
         response = sqs.send_message(
             QueueUrl=qurl,
-            MessageBody=message
+            MessageBody=message,
+            MessageAttributes={
+                'project': {
+                    'StringValue': 'research',
+                    'DataType': 'String'
+                }
+            }
         )
         print(response)
+        status = response['ResponseMetadata']['HTTPStatusCode']
+        if (status == 200):
+            print("200 OK")
+        else:
+            print("Not-OK")
     except ClientError as e:
         print(e)
 
