3603b222304b75a5dfc6e6be06a6115ac34e15c3
Neal Magee <nem2p@virginia.edu>
2026-04-08 11:43:47 -0400

    Tracking aws secrets manager

---
+++ secrets/fetch-secret.py
@@ -0,0 +1,33 @@
+import boto3
+from botocore.exceptions import ClientError
+
+def get_secret():
+
+    secret_name = "fname"
+    region_name = "us-east-1"
+
+    # Create a Secrets Manager client
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        # For a list of exceptions thrown, see
+        # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+        raise e
+
+    print("Full secret response:")
+    print(get_secret_value_response)
+    secret = get_secret_value_response['SecretString']
+
+    print("Isolated secret string:")
+    print(secret)
+    # Your code goes here.
+
+get_secret()
